5a35ce059cf93087390505c48e5ddbd8eb16ee98
Keegan Dwyer <kdwyer@localhost.localdomain>
2017-03-22 19:46:03 -0500

    Initial Commit to setup folder for Lynda Tutorial

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'mw-app',
+  template: `
+    '<h1>My App</h1>
+    <p>Keeping Track of the Media I want to watch.</p>'
+    `
+
+})
+export class AppComponent {}
